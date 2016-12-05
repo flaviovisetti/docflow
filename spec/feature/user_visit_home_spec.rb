cc08797119dbb7fc4bd97672858168f0d90a97ad
Flavio e Rodrigo <flavio.visetti@gmail.com>
2016-12-05 20:02:51 -0200

    "Include the description of website"

--- spec/feature/user_visit_home_spec.rb
+++ spec/feature/user_visit_home_spec.rb
@@ -6,4 +6,12 @@ feature 'user visit home page' do
 
     expect(page).to have_css('h1', text: 'Welcome a board')
   end
+  scenario 'succesfully read product presentation' do
+    visit root_path
+
+    expect(page).to have_css('h1', text: 'Welcome a board')
+    expect(page).to have_content ('Bem-vindo! As interações entre seu
+      time acabaram de ficar mais fáceis e ágeis,
+      atravez de uma interface intuitiva e com vários recursos.')
+  end
 end
