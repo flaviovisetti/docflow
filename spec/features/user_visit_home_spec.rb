424e859bf598189c3b667c04b77bbab40b1a0850
Flavio Visetti <flavio.visetti@gmail.com>
2016-12-11 18:03:05 -0200

    fix the writing

--- spec/features/user_visit_home_spec.rb
+++ spec/features/user_visit_home_spec.rb
@@ -4,13 +4,13 @@ feature 'user visit home page' do
   scenario 'succesfully to visit' do
     visit root_path
 
-    expect(page).to have_css('h1', text: 'Welcome a board')
+    expect(page).to have_css('h1', text: 'Welcome aboard')
   end
 
   scenario 'succesfully read product presentation' do
     visit root_path
 
-    expect(page).to have_css('h1', text: 'Welcome a board')
+    expect(page).to have_css('h1', text: 'Welcome aboard')
     expect(page).to have_content('Bem-vindo! As interações entre seu
       time acabaram de ficar mais fáceis e ágeis,
       através de uma interface intuitiva e com vários recursos.')
